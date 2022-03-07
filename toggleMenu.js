31622684b232e43e851339d4bac0c35d21b62f2e
tempie <63066656+imTempie@users.noreply.github.com>
2022-03-07 22:27:40 +0000

    Update and rename script.js to toggleMenu.js

---
+++ toggleMenu.js
@@ -0,0 +1,11 @@
+var toggleMenu = () => {
+  let bm = document.getElementsByClassName("burgerMenu")[0]; 
+  if (bm.classList.contains("open")) 
+  {
+    bm.classList.remove("open");
+  } 
+  else
+  {
+    bm.classList.add("open");
+  }
+};
