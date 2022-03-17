640ba6cee0fbad811c63ee4a2ada2c79b237c1bf
tempie <63066656+imTempie@users.noreply.github.com>
2022-03-17 21:22:18 +0000

    Update toggleMenu.js

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,12 +1,11 @@
 var toggleMenu = () => {
-  
   let bm = document.getElementsByClassName("burgerMenu")[0]; 
-  
-  if (bm.classList.contains("open")) {
+  if (bm.classList.contains("open")) 
+  {
     bm.classList.remove("open");
   } 
-  else {
+  else
+  {
     bm.classList.add("open");
   }
-  
 };
